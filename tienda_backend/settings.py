368317bcd71926503a5e0feba9e72de7a7c99236
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-08-28 18:32:54 -0500

    Update settings.py

--- tienda_backend/settings.py
+++ tienda_backend/settings.py
@@ -138,6 +138,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True 
 
+
 # ✅ Configuración de archivos multimedia
 
 MEDIA_ROOT = BASE_DIR / 'media'
